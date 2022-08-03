d5f1c9cd6f6d5ad699af19a4c917b02007a94137
sethupathyrv <sethupathyrv@gmail.com>
2022-08-03 11:49:39 +0530

    add initial repo structure and factory

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("initial commit")
+import sys
+from sly import Parser
+from Language.Python.Python_MakeParser import Python_MakeParser
+from Language.C.C_MakeParser import C_MakeParser
+
+
+def Factory(lang, formalStructures):
+    if lang == "C":
+        Parser = C_MakeParser(formalStructures)
+    elif lang == "Python":
+        Parser = Python_MakeParser(formalStructures)
+    else:
+        print("Parser not found")
+
+
+if __name__ == "__main__":
+    f = open(sys.argv[2], "r")
+    file = f.readlines()
+    Factory(sys.argv[1], file)
