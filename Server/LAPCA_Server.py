b9cff04049771dd721b115d6c2904ae86d38a411
Soupy710 <swaroopbhat710@gmail.com>
2022-09-27 16:59:48 +0530

    fixed return issue

--- Server/LAPCA_Server.py
+++ Server/LAPCA_Server.py
@@ -1,4 +1,5 @@
 #Flask server for LAPCA
+from re import S
 from flask import request, jsonify
 from flask import Flask
 import sys
@@ -12,13 +13,16 @@ mapping['Assign in loop'] = './Guidelines/Assign_in_loop.lapx'
 
 
 def accessRes(file,code,form,res):
+    s = ""
     with open(file, "w") as text_file:
         text_file.write(code)
     for guideline in form.keys():
         if form[guideline] :
             MainModule(mapping[guideline], file).factory()
             with open("results.txt", "r") as text_file:
-                res+= text_file.read()
+                s+=text_file.read()
+    return s
+    
 
 @app.route('/getResults', methods=['POST'])
 def getResults():
@@ -28,11 +32,12 @@ def getResults():
     form = data['form']
     res = ""
     if language == 'Python':
-        accessRes("Server/test.py",code,form,res)
+        res+=accessRes("Server/test.py",code,form,res)
     elif language == 'C':
-        accessRes("Server/test.c",code,form,res)
+        res+=accessRes("Server/test.c",code,form,res)
     elif language == 'Java':
-        accessRes("Server/test.java",code,form,res)
+        res+=accessRes("Server/test.java",code,form,res)
+    print(res)
     return jsonify(res)
 
 if __name__ == '__main__':
