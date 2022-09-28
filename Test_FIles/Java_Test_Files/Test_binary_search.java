19f97f0739975914a644dfbca751ada0328de1fc
Soupy710 <swaroopbhat710@gmail.com>
2022-09-28 18:26:54 +0530

    added test files and modified few test files

---
+++ Test_FIles/Java_Test_Files/Test_binary_search.java
@@ -0,0 +1,35 @@
+//iterative function that implements binary search
+public class Test_binary_search {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int x = 5;
+        int result = binarySearch(arr, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+
+    // Returns index of x if it is present in arr[l.. r], else return -1
+    static int binarySearch(int[] arr, int x) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            // Check if x is present at mid
+            if (arr[m] == x)
+                return m;
+
+            // If x greater, ignore left half
+            if (arr[m] < x)
+                l = m + 1;
+
+                // If x is smaller, ignore right half
+            else
+                r = m - 1;
+        }
+
+        // if we reach here, then element was not present
+        return -1;
+    }
+}
---
+++ Test_FIles/Python_Test_Files/Test_binary_search.py
@@ -0,0 +1,16 @@
+#iterative function to implement binary search
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+print(binary_search([1,2,3,4,5,6,7,8,9,10], 5))
