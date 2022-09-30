2855c6c47d7a8c1aa92fa5f30158f5f77fc83951
Manish Reddy <manishreddym22@gmail.com>
2022-09-30 13:03:14 +0530

    Fix path issues

--- Server/LAPCA_Server.py
+++ Server/LAPCA_Server.py
@@ -5,9 +5,6 @@ import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from main import MainModule
 app = Flask(__name__)
-mapping = {}
-mapping['Dead code'] = os.path.abspath('./Guidelines/Dead_Code.lapx')
-mapping['Assign in loop'] = os.path.abspath('./Guidelines/Assign_in_loop.lapx')
 
 def accessRes(file,code,form):
     s = ""
@@ -15,7 +12,7 @@ def accessRes(file,code,form):
         text_file.write(code)
     for guideline in form.keys():
         if form[guideline] :
-            MainModule(mapping[guideline], file).factory()
+            MainModule(os.path.join("Guidelines",guideline),file).factory()
             with open("results.txt", "r") as text_file:
                 s+=text_file.read()
     return s    
