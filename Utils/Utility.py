c98011921452f5ec62e77f9b51b180452e31bf75
Soupy710 <swaroopbhat710@gmail.com>
2022-10-26 22:18:53 +0530

    updated common functions structure

--- Utils/Utility.py
+++ Utils/Utility.py
@@ -1,8 +1,5 @@
 import os
-import sys
 from lark.lexer import Token
-from lark import Lark, visitors, tree
-
 class Utility:
     def __init__(self,lang):
         self.lang = lang
@@ -14,14 +11,13 @@ class Utility:
         elif lang=="py":
             self.utilityObject = pythonUtilities()
 
-class getTokens:
-    def __init__(self,Tree,token_list):
-        if isinstance(Tree,Token):
-            token_list.append(Tree.value)
-        elif Tree:
-            l = Tree.children
-            for i in l:  
-                getTokens(i,token_list)
+def getTokens(Tree,token_list):
+    if isinstance(Tree,Token):
+        token_list.append(Tree.value)
+    elif Tree:
+        l = Tree.children
+        for i in l:  
+            getTokens(i,token_list)
 
 class pythonUtilities():
     def __init__(self):
